de3d1b5d240a5b6ef0699b5c93ab687f17ac586f
thealphakenya <thealphakenya@gmail.com>
2025-04-07 01:35:47 +0300

    Your commit message

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 const API_TOKEN = process.argv[2];
-const SERVICE_ID = "ee4bdd61-21d2-4bde-bba4-4be716d1ed9d"; // from URL
-const PROJECT_ID = "6794a5bd-f6b0-46b2-a199-506694340d97"; // from URL
+const PROJECT_ID = "6794a5bd-f6b0-46b2-a199-506694340d97";
+const SERVICE_ID = "ee4bdd61-21d2-4bde-bba4-4be716d1ed9d";
 
 async function redeploy() {
   const query = `
@@ -17,29 +17,25 @@ async function redeploy() {
   const variables = {
     input: {
       projectId: PROJECT_ID,
-      serviceId: SERVICE_ID,
+      serviceId: SERVICE_ID
     }
   };
 
-  const response = await fetch("https://backboard.railway.app/graphql/v2", {
+  const res = await fetch("https://backboard.railway.app/graphql/v2", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${API_TOKEN}`,
+      "Authorization": `Bearer ${API_TOKEN}`,
     },
-    body: JSON.stringify({
-      query,
-      variables
-    }),
+    body: JSON.stringify({ query, variables }),
   });
 
-  const data = await response.json();
-
-  if (data.errors) {
-    console.error("❌ Redeploy failed:", data.errors);
+  const json = await res.json();
+  if (json.errors) {
+    console.error("❌ Redeploy failed:", json.errors);
     process.exit(1);
   } else {
-    console.log("✅ Redeploy successful:", data.data.serviceInstanceRedeploy);
+    console.log("✅ Redeploy successful:", json.data.serviceInstanceRedeploy);
   }
 }
 
